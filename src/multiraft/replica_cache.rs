d3ca722ddbf7f9fc6e4e2cbb0de0e416d456f162
future <jackson.libc.rs@gmail.com>
2023-01-04 18:38:57 +0800

    refactor replica cache for storage

--- src/multiraft/replica_cache.rs
+++ src/multiraft/replica_cache.rs
@@ -1,74 +1,16 @@
 use std::collections::HashMap;
 use std::marker::PhantomData;
 
-use tokio::sync::RwLock;
+use prost::encoding::group;
 
-use crate::proto::ReplicaMetadata;
+use crate::proto::RaftGroupDesc;
+use crate::proto::ReplicaDesc;
 use crate::storage::MultiRaftStorage;
 use crate::storage::RaftStorage;
 
 use super::error::Error;
 
-/// ReplicaMetadataCache is a thread safe cache. it cache replica metadatas
-/// from read storage and messages and write the replica metadata the storage
-/// when cache miss.
-pub struct ReplicaMetadataCache<RS, MRS>
-where
-    RS: RaftStorage,
-    MRS: MultiRaftStorage<RS>,
-{
-    storage: MRS,
-    inner: RwLock<HashMap<(u64, u64), ReplicaMetadata>>,
-    _m: PhantomData<RS>,
-}
-
-impl<RS, MRS> ReplicaMetadataCache<RS, MRS>
-where
-    RS: RaftStorage,
-    MRS: MultiRaftStorage<RS>,
-{
-    pub fn new(storage: MRS) -> Self {
-        Self {
-            storage,
-            inner: Default::default(),
-            _m: PhantomData,
-        }
-    }
-
-    pub async fn get(
-        &self,
-        group_id: u64,
-        replica_id: u64,
-    ) -> Result<ReplicaMetadata, Error> {
-        {
-            let rl = self.inner.read().await;
-            if let Some(replica_metadata) = rl.get(&(group_id, replica_id)) {
-                return Ok(replica_metadata.clone());
-            }
-        }
-
-        self.storage.replica_metadata(group_id, replica_id).await.map_err(|err| Error::Store(err))
-    }
-
-    pub async fn cache(&self, group_id: u64, replica_metadata: ReplicaMetadata) {
-        assert_ne!(replica_metadata.replica_id, 0);
-        let mut wl = self.inner.write().await;
-        let key = (group_id, replica_metadata.replica_id);
-        match wl.get_mut(&key) {
-            None => {
-                wl.insert(key, replica_metadata);
-            }
-            Some(old) => {
-                if *old != replica_metadata {
-                    std::mem::replace(old, replica_metadata);
-                }
-            }
-        }
-    }
-}
-
-
-/// ReplicaMetadataCache is a thread safe cache. it cache replica metadatas
+/// ReplicaCache cache replica metadatas
 /// from read storage and messages and write the replica metadata the storage
 /// when cache miss.
 pub struct ReplicaCache<RS, MRS>
@@ -77,7 +19,7 @@ where
     MRS: MultiRaftStorage<RS>,
 {
     storage: MRS,
-    group_replicas: HashMap<u64, Vec<ReplicaMetadata>>,
+    groups: HashMap<u64, RaftGroupDesc>,
     _m: PhantomData<RS>,
 }
 
@@ -89,48 +31,96 @@ where
     pub fn new(storage: MRS) -> Self {
         Self {
             storage,
-            inner: Default::default(),
+            groups: Default::default(),
             _m: PhantomData,
         }
     }
 
-    pub async fn get(
-        &self,
+    /// Get replica description from this cache when replica_id equals, 
+    /// return `Error` if storage error occurs. if group is not in the 
+    /// cache, it is read from storage and if storage is not found too, 
+    /// `None` is returned.
+    pub async fn replica_desc(
+        &mut self,
         group_id: u64,
         replica_id: u64,
-    ) -> Result<ReplicaMetadata, Error> {
-        {
-            let rl = self.inner.read().await;
-            if let Some(replica_metadata) = rl.get(&(group_id, replica_id)) {
-                return Ok(replica_metadata.clone());
-            }
-        }
-
-        self.storage.replica_metadata(group_id, replica_id).await.map_err(|err| Error::Store(err))
+    ) -> Result<Option<ReplicaDesc>, Error> {
+        let _ = self.ensure_cache_group(group_id).await?;
+        let group_desc = self.groups.get(&group_id).unwrap();
+        Ok(
+            ReplicaCache::<RS, MRS>::find(group_desc, |replica| replica.replica_id == replica_id)
+                .await,
+        )
     }
 
-    pub async fn cache(&mut self, group_id: u64, replica_metadata: ReplicaMetadata) {
-        assert_ne!(replica_metadata.replica_id, 0);
-        if let Some(replicas) = self.group_replicas.get_mut(&group_id) {
-            if replicas.iter().find(|replica| **replica == replica_metadata).is_some() {
+    /// Get replica description from this cache when node_id equals, 
+    /// return `Error` if storage error occurs. if group is not in the 
+    /// cache, it is read from storage and if storage is not found too, 
+    /// `None` is returned.
+    pub async fn replica_for_node(
+        &mut self,
+        group_id: u64,
+        node_id: u64,
+    ) -> Result<Option<ReplicaDesc>, Error> {
+        let _ = self.ensure_cache_group(group_id).await?;
+        let group_desc = self.groups.get(&group_id).unwrap();
+        Ok(ReplicaCache::<RS, MRS>::find(group_desc, |replica| replica.node_id == node_id).await)
+    }
+
+    pub async fn cache_replica_desc(
+        &mut self,
+        group_id: u64,
+        replica_desc: ReplicaDesc
+    ) {
+        if let Some(group) = self.groups.get_mut(&group_id) {
+            if group.replicas.iter().find(|replica| **replica == replica_desc).is_some() {
                 return
             }
 
-            replicas.push(replica_metadata);
-            // TODO: update storage
+            // update cache
+            group.nodes.push(replica_desc.node_id);
+            group.replicas.push(replica_desc);
+
+            // TODO: write storage
+            // TODO: add sync option
+            // TODO: direact update storage
             return
         }
-        
-        let key = (group_id, replica_metadata.replica_id);
-        match wl.get_mut(&key) {
-            None => {
-                wl.insert(key, replica_metadata);
-            }
-            Some(old) => {
-                if *old != replica_metadata {
-                    std::mem::replace(old, replica_metadata);
-                }
-            }
+
+        let mut group_desc = RaftGroupDesc::default();
+        group_desc.nodes.push(replica_desc.node_id);
+        group_desc.replicas.push(replica_desc);
+        // TODO: direact create new group_desc in storage
+        return
+    }
+
+    #[inline]
+    async fn ensure_cache_group(&mut self, group_id: u64) -> Result<(), Error> {
+        if self.groups.get(&group_id).is_none() {
+            let group_desc = self
+                .storage
+                .group_desc(group_id)
+                .await
+                .map_err(|err| Error::Store(err))?;
+            self.groups.insert(group_id, group_desc);
         }
+
+        Ok(())
+    }
+
+    #[inline]
+    async fn find<P>(group_desc: &RaftGroupDesc, predicate: P) -> Option<ReplicaDesc>
+    where
+        P: Fn(&&ReplicaDesc) -> bool,
+    {
+        if group_desc.replicas.is_empty() {
+            return None;
+        }
+
+        if let Some(replica) = group_desc.replicas.iter().find(predicate) {
+            return Some(replica.clone());
+        }
+
+        None
     }
 }
