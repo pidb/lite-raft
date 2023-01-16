a2311bb550a7e379e7dce5fbc10ac111832b402f
future <jackson.libc.rs@gmail.com>
2023-01-17 00:21:17 +0800

    REFACTOR: transport

--- oceanraft/src/multiraft/transport.rs
+++ oceanraft/src/multiraft/transport.rs
@@ -1,17 +1,20 @@
 use futures::Future;
 
 use raft::Storage;
-use tracing::info;
+use tracing::error;
+use tracing::trace;
+use tracing::warn;
+use tracing::Level;
 
-use super::error::Error;
-use super::multiraft::Context;
-use super::node::NodeManager;
-
-use super::storage::MultiRaftStorage;
 use raft_proto::prelude::Message;
 use raft_proto::prelude::MessageType;
 use raft_proto::prelude::RaftMessage;
 use raft_proto::prelude::RaftMessageResponse;
+
+use super::error::Error;
+use super::node::NodeManager;
+use super::replica_cache::ReplicaCache;
+use super::storage::MultiRaftStorage;
 
 pub trait RaftMessageDispatch: Send + Sync + 'static {
     type DispatchFuture<'life0>: Future<Output = Result<RaftMessageResponse, Error>> + Send
@@ -27,8 +30,8 @@ pub trait Transport: Send + Sync + 'static {
 
 pub async fn send_messages<TR, RS, MRS>(
     from_node_id: u64,
-    storage: &MRS,
     transport: &TR,
+    replica_cache: &mut ReplicaCache<RS, MRS>,
     node_mgr: &mut NodeManager,
     group_id: u64,
     msgs: Vec<Message>,
@@ -37,28 +40,45 @@ pub async fn send_messages<TR, RS, MRS>(
     RS: Storage,
     MRS: MultiRaftStorage<RS>,
 {
+    assert_ne!(from_node_id, 0);
     for msg in msgs {
         match msg.msg_type() {
             MessageType::MsgHeartbeat => {
-                info!(
-                    "node {} drop indvidual heartbeat message to replica {}",
-                    from_node_id, msg.to
+                trace!(
+                    "node({}) group({}) replica({}) drop individual heartbeat message to replica({})",
+                    from_node_id, group_id, msg.from, msg.to
                 );
             }
             MessageType::MsgHeartbeatResponse => {
-                info!(
-                    "node {} drop indvidual heartbeat response message to replica {}",
-                    from_node_id, msg.to
+                trace!(
+                    "node({}) group({}) replica({}) drop individual heartbeat message to replica({})",
+                    from_node_id, group_id, msg.from, msg.to
                 );
             }
-            _ => send_message(storage, transport, node_mgr, group_id, msg).await,
+            _ => {
+                send_message(
+                    from_node_id,
+                    transport,
+                    replica_cache,
+                    node_mgr,
+                    group_id,
+                    msg,
+                )
+                .await
+            }
         }
     }
 }
 
-pub async fn send_message<TR, RS, MRS>(
-    storage: &MRS,
+#[tracing::instrument(
+    level = Level::TRACE,
+    name = "transport::send_message",
+    skip(replica_cache, transport, node_mgr, group_id)
+)]
+async fn send_message<TR, RS, MRS>(
+    from_node_id: u64,
     transport: &TR,
+    replica_cache: &mut ReplicaCache<RS, MRS>,
     node_mgr: &mut NodeManager,
     group_id: u64,
     msg: Message,
@@ -67,34 +87,47 @@ pub async fn send_message<TR, RS, MRS>(
     RS: Storage,
     MRS: MultiRaftStorage<RS>,
 {
-    let to_replica = storage
-        .replica_desc(group_id, msg.to)
-        .await
-        .unwrap()
-        .unwrap();
+    // if we lose information about the target replica, we may not be able to
+    // send any messages other than heartbeats, which may cause the raft group
+    // quorum to be temporarily unavailable.
+    //
+    // but this is rare, and if it does happen, it is fixed later by
+    // synchronization (TODO: unimpl)
+    let to_replica = match replica_cache.replica_desc(group_id, msg.to).await {
+        Err(err) => {
+            warn!(
+                "find to node replica description error {} for send {:?}",
+                err, msg
+            );
+            return;
+        }
+        Ok(op) => match op {
+            None => {
+                warn!(
+                    "can not find to node replica description for send {:?}",
+                    msg
+                );
+                return;
+            }
+            Some(data) => data,
+        },
+    };
     assert_ne!(to_replica.node_id, 0);
 
-    let from_replica = storage
-        .replica_desc(group_id, msg.from)
-        .await
-        .unwrap()
-        .unwrap();
-    assert_ne!(from_replica.node_id, 0);
-
-    info!(
-        "from_node {} -> to_node {} send message {:?}",
-        from_replica.node_id, to_replica.node_id, msg
-    );
     if !node_mgr.contains_node(&to_replica.node_id) {
         node_mgr.add_node(to_replica.node_id, group_id);
     }
 
     let msg = RaftMessage {
         group_id,
-        from_node: from_replica.node_id,
+        from_node: from_node_id,
         to_node: to_replica.node_id,
         replicas: vec![],
         msg: Some(msg),
     };
-    transport.send(msg).unwrap();
+
+    // FIXME: send trait should be return original msg when error occurred.
+    if let Err(err) = transport.send(msg) {
+        error!("call transport error {} for send msg", err);
+    }
 }
