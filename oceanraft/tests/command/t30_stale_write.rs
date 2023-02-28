0995f05c543d50de561e00d988c09d95bda8b735
future <jackson.libc.rs@gmail.com>
2023-02-28 15:54:02 +0800

    FIX: stale write test when `max_size_per_msg` is 0

--- oceanraft/tests/command/t30_stale_write.rs
+++ oceanraft/tests/command/t30_stale_write.rs
@@ -1,10 +1,12 @@
 use std::time::Duration;
 
+use tokio::time::timeout_at;
+use tokio::time::Instant;
 
-use crate::fixtures::WriteChecker;
 use crate::fixtures::init_default_ut_tracing;
 use crate::fixtures::quickstart_group;
 use crate::fixtures::FixtureCluster;
+use crate::fixtures::WriteChecker;
 
 /// Testing pending proposals after removing the leader of
 /// a single consensus group should return an error。
@@ -24,13 +26,13 @@ async fn test_group_stale_write() {
     let (_task_group, mut cluster) = quickstart_group(node_nums).await;
 
     let group_id = 1;
-   
+
     // disconnect node 1 network
     cluster.transport.disconnect(1, 2).await;
     cluster.transport.disconnect(1, 3).await;
 
     // propose command, but node network is disconnected, so command can't commit.
-    let command_size = 100;
+    let command_size = 10;
     let mut stale_recvs = vec![];
     for command_id in 1..command_size + 1 {
         let data = format!("{}: data on group_id = {}", command_id, group_id)
@@ -45,45 +47,52 @@ async fn test_group_stale_write() {
 
     cluster.campaign_group(2, group_id).await;
     for i in 1..3 {
-        let el = FixtureCluster::wait_leader_elect_event(&mut cluster, i+1)
-        .await
-        .unwrap();
+        let el = FixtureCluster::wait_leader_elect_event(&mut cluster, i + 1)
+            .await
+            .unwrap();
         assert_eq!(el.leader_id, 2);
-    }
-
-
-    let mut write_checker = WriteChecker::default();
-    let mut recvs = vec![];
-    for command_id in command_size+1..command_size+command_size+1 {
-        let data = format!("{}: data on group_id = {}", command_id, group_id)
-            .as_bytes()
-            .to_vec();
-
-        write_checker.insert_write(group_id, data.clone());
-        recvs.push(cluster.write_command(2, group_id, data));
-        // TODO: use tick method
-        cluster.tickers[1].tick().await;
-    }
-
-    let apply_events = FixtureCluster::wait_for_command_apply(cluster.mut_apply_event_rx(2), Duration::from_millis(1000), 100).await.unwrap();
-    write_checker.check(&apply_events);
-    for event in apply_events {
-        // TODO: use done method
-        event.tx.map(|tx| tx.send(Ok(())));
     }
 
     cluster.transport.reconnect(1, 2).await;
     cluster.transport.reconnect(1, 3).await;
 
+    // check stale
+    let mut write_checker = WriteChecker::default();
+    let mut recvs = vec![];
+    for (i, stale_rx) in stale_recvs.into_iter().enumerate() {
+        // because heartbeat can not set committed index, so whenever we
+        // are ready to verify stale command, we need send append to active
+        // commit of stale node.
+        let data = format!("{}: data on group_id = {}", i, group_id)
+            .as_bytes()
+            .to_vec();
+        write_checker.insert_write(group_id, data.clone());
+        recvs.push(cluster.write_command(2, group_id, data));
 
-    for stale_rx in stale_recvs {
         // TODO: use tick method
-        cluster.tickers[0].tick().await;
+        // cluster.tickers[0].tick().await;
         cluster.tickers[1].tick().await;
-        cluster.tickers[2].tick().await;
+        // cluster.tickers[2].tick().await;
         // TODO: assertion error type
+        // let res = timeout_at(Instant::now() + Duration::from_millis(1000), stale_rx)
+        // .await
+        // .expect(format!("wait stale {} timeouted", i + 1).as_str());
         assert_eq!(stale_rx.await.unwrap().is_err(), true);
-    } 
+    }
+
+    // check normal
+    let apply_events = FixtureCluster::wait_for_command_apply(
+        cluster.mut_apply_event_rx(2),
+        Duration::from_millis(1000),
+        command_size,
+    )
+    .await
+    .unwrap();
+    write_checker.check(&apply_events);
+    for event in apply_events {
+        // TODO: use done method
+        event.tx.map(|tx| tx.send(Ok(())));
+    }
 
     for rx in recvs {
         // TODO: assertiong response type
